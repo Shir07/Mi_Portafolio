dcc52f44b273517c89f5c46082f447b46255105f
Shir07 <priscila.ohannecian7@gmail.com>
2023-04-10 21:55:41 -0300

    Se crea proyectos

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     ExperienciaComponent,
     EducacionComponent,
     SkillsComponent,
+    ProyectosComponent,
    
   ],
   imports: [
