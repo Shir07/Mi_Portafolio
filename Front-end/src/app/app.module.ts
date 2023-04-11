18374726ec1b2c7fd26a6ea7f866692718281b9d
Shir07 <priscila.ohannecian7@gmail.com>
2023-04-11 19:22:16 -0300

    Se modifica footer

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
     EducacionComponent,
     SkillsComponent,
     ProyectosComponent,
+    FooterComponent,
    
   ],
   imports: [
